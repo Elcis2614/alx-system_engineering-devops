692b310ae50ed21139cd081de181f1f6ff797166
elcis2614 <elisereflorbusole@gmail.com>
2023-05-18 08:41:48 +0000

    message

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+*[0-9a-z]*)\] \[to:(\+*\d+)\] \[flags:(.*\d)\]/i).join(",")
+puts ARGV[0].scan(/\[from:(\+*[0-9a-z]*)\] \[to:(\+*\d+)\] \[flags:(.*\d).\s/i).join(",")
