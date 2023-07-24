724c191770d04f7a92901ef199e6ed6deb4be646
elcis2614 <elisereflorbusole@gmail.com>
2023-07-24 16:22:37 +0000

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Given the API, for a given employee ID, returns information about his/her TODO list progress. """
+""" Given the API, for a given employee ID, returns information
+about his/her TODO list progress. """
 import json
 import sys
 import urllib.request as lib
@@ -17,15 +18,16 @@ if __name__ == "__main__":
         if (cust_id == int(a_dict["id"])):
             custName = a_dict["name"]
             break
-    customer = {"name": custName, "tasks": [], "maxTask" : 0, "doneTask" : 0}
+    customer = {"name": custName, "tasks": [], "maxTask": 0, "doneTask": 0}
 
-    for a_dict in data :
+    for a_dict in data:
         if (a_dict["userId"] == cust_id):
-            customer["maxTask"] += 1;
-            if a_dict["completed"] :
+            customer["maxTask"] += 1
+            if a_dict["completed"]:
                 customer["tasks"].append(a_dict["title"])
                 customer["doneTask"] += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(custName, customer["doneTask"], customer["maxTask"]))
+    print("Employee {} is done with tasks({}/{}):".format(
+        custName, customer["doneTask"], customer["maxTask"]))
     for task in customer["tasks"]:
         print("\t {}".format(task))
