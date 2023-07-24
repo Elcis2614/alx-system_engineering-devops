600a6991f6d16343837280d88244aad4217b1b79
elcis2614 <elisereflorbusole@gmail.com>
2023-07-24 22:15:58 +0000

    dicitionary of all

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Given the API, for a given employee ID, returns information
+about his/her TODO list progress. """
+
+import json
+import urllib.request as lib
+
+if __name__ == "__main__":
+    dataSrc = lib.urlopen('https://jsonplaceholder.typicode.com/todos')
+    data = json.loads(dataSrc.read())
+
+    userSrc = lib.urlopen('https://jsonplaceholder.typicode.com/users')
+    users = json.loads(userSrc.read())
+
+    userDict = {}
+    for a_dict in users:
+        if (a_dict["id"] not in userDict.keys()):
+            userDict[a_dict["id"]] = a_dict["username"]
+
+    allDict = {}
+    for a_dict in data:
+        userId = a_dict["userId"]
+        if (userId in allDict.keys()):
+            allDict[userId].append({
+                "username": userDict[userId],
+                "task": a_dict["title"],
+                "completed": a_dict["completed"]})
+        else :
+            allDict[userId] = [{
+                "username": userDict[userId],
+                "task": a_dict["title"],
+                "completed": a_dict["completed"]}]
+        
+    with open('todo_all_employees.json', 'w') as mFile:
+        json.dump(allDict, mFile)
